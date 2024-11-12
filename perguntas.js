b1cd71973fb3f5d98c902f66f8f05bb87e1189a2
Henrique549y <henrique10millegal@gmail.com>
2024-11-12 15:51:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'é a linguagem usada para criar e estruturar páginas web. Com ele, você define o conteúdo de uma página, como texto, imagens e links. É como o esqueleto de um site.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual cidade é a capital da Rússia?',
+    'A capital da Rússia é Moscou'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é um número primo?',
+    'Um número primo é um número maior que 1 que só pode ser dividido por 1 e por ele mesmo.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz até mais em Inglês?',
+    'Até mais em ingles é See you later (RAI)'
+)
